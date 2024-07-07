a7a251db040abb55bf7cdab9895109ab2c8c60a1
Kwayu Mmari <mmaridenis@gmail.com>
2024-07-07 17:32:38 +0300

    fixed menu

--- utils/header/header-upper.php
+++ utils/header/header-upper.php
@@ -16,7 +16,7 @@
 							<div class="upper-column info-box">
 								<div class="icon-box"><span class="flaticon-24-hours-2"></span></div>
 								<ul>
-									<li><span>Abla complex,</span> <br> Dar es Salaam, Tanzania</li>
+									<li><span>Same Ground,</span> <br> Morogoro, Tanzania</li>
 								</ul>
 							</div>
 							
@@ -24,17 +24,17 @@
 							<div class="upper-column info-box">
 								<div class="icon-box"><span class="flaticon-call"></span></div>
 								<ul>
-									<li><span>+ 255 762 996 305 </span> <br> info@muslimwomen.or.tz</li>
+									<li><span>+ 255 762 996 305 </span> <br> samegroundtanzania@gmail.com</li>
 								</ul>
 							</div>
 							
 							<!--Info Box-->
 							<div class="upper-column info-box">
 								<ul class="social-icon-two">
-									<li><a href="https://www.facebook.com/muslimwomenawardstz/" style="background-color : #000000; color: #ffffff"><span class="fa fa-facebook-f"></span></a></li>
-									<li><a href="https://www.instagram.com/muslimwomenawardstz/" style="background-color : #000000; color: #ffffff"><span class="fa fa-instagram"></span></a></li>
-									<li><a href="https://www.twitter.com/muslimwomenawardstz/" style="background-color : #000000; color: #ffffff"><span class="fa fa-twitter"></span></a></li>
-									<li><a href="https://www.youtube.com/muslimwomenawardstz/" style="background-color : #000000; color: #ffffff"><span class="fa fa-youtube"></span></a></li>
+									<li><a href="https://www.facebook.com/samegroundtanzania/" style="background-color : #000000; color: #ffffff"><span class="fa fa-facebook-f"></span></a></li>
+									<li><a href="https://www.instagram.com/samegroundtanzania/" style="background-color : #000000; color: #ffffff"><span class="fa fa-instagram"></span></a></li>
+									<li><a href="https://www.twitter.com/samegroundtanzania/" style="background-color : #000000; color: #ffffff"><span class="fa fa-twitter"></span></a></li>
+									<li><a href="https://www.youtube.com/samegroundtanzania/" style="background-color : #000000; color: #ffffff"><span class="fa fa-youtube"></span></a></li>
 								</ul>
 							</div>
                         </div>
