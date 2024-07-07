77faa3f01b139538b24804d7ba5c7192cf506526
Kwayu Mmari <mmaridenis@gmail.com>
2024-07-08 02:19:08 +0300

    fixed menu

--- utils/slider/index.php
+++ utils/slider/index.php
@@ -1,36 +1,31 @@
-<?php
-include 'cms/connection/index.php';
-  $query="select * from carousel";
-  $result4=mysqli_query($connect,$query);
-  // $count=mysqli_fetch_assoc($result4);
-  $total_users = mysqli_num_rows($result4);
-
- ?>
-<!--Main Slider-->
 <section class="main-slider style-two">
         <div class="main-slider-carousel owl-carousel owl-theme">
-        <?php   // LOOP TILL END OF DATA
-                                                    while($rows4=$result4->fetch_assoc())
-                                                         {
-                                                       ?>
+
             <div class="slide" style="background-image:url(cms/cv/<?php echo $rows4['image'];?>)">
                 <div class="auto-container">
                 	<div class="content">
                     <h2><?php echo $rows4['title1'];?></h2>
-						<div class="text"><?php echo $rows4['titles'];?></div>
+						<div class="text">To improve the student learning experience by ensuring each pupil has a fair start. Be a part of our projects</div>
                     	<div class="link-box">
 							<a href="about.html" class="theme-btn btn-style-one"><span class="txt">Nominate Now</span></a>
 						</div>
                     </div>
                 </div>
             </div>
-            
-			<?php
-                                                     }
-                                                     ?>
+
+            <div class="slide" style="background-image:url(cms/cv/<?php echo $rows4['image'];?>)">
+                <div class="auto-container">
+                	<div class="content">
+                    <h2><?php echo $rows4['title1'];?></h2>
+						<div class="text">To improve the student learning experience by ensuring each pupil has a fair start. Be a part of our projects.</div>
+                    	<div class="link-box">
+							<a href="about.html" class="theme-btn btn-style-one"><span class="txt">Nominate Now</span></a>
+						</div>
+                    </div>
+                </div>
+            </div>
+
+
         </div>
-		
-		<!--Scroll Dwwn Btn-->
         <div class="mouse-btn-down scroll-to-target" data-target=".what-we-section"></div>
     </section>
-    <!--End Main Slider-->
