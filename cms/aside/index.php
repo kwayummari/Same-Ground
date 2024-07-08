478a41149bf8b99da88c913d2ef771e0327d1e4a
Kwayu Mmari <mmaridenis@gmail.com>
2024-07-08 12:21:30 +0300

    fixed menu

--- cms/aside/index.php
+++ cms/aside/index.php
@@ -1,15 +1,11 @@
-<!-- ======= Sidebar ======= -->
   <aside id="sidebar" class="sidebar">  
-
     <ul class="sidebar-nav" id="sidebar-nav">
-
       <li class="nav-item">
         <a class="nav-link " href="../home/">
           <i class="bi bi-grid"></i>
           <span>Dashboard</span>
         </a>
       </li>
-      <!-- End Dashboard Nav -->
 
       <li class="nav-item">
         <a class="nav-link collapsed" data-bs-target="#tables-nav" data-bs-toggle="collapse" href="">
@@ -22,37 +18,11 @@
             </a>
           </li>
           <li>
-            <a href="../carousel/">
-              <i class="bi bi-circle"></i><span>Carousel</span>
-            </a>
-          </li>
-          <li>
             <a href="../blog/">
               <i class="bi bi-circle"></i><span>Blog</span>
             </a>
           </li>
-          <li>
-            <a href="../winners/">
-              <i class="bi bi-circle"></i><span>Winners</span>
-            </a>
-          </li>
-          <li>
-            <a href="../categories/">
-              <i class="bi bi-circle"></i><span>Categories</span>
-            </a>
-          </li>
-          <li>
-            <a href="../testimonial/">
-              <i class="bi bi-circle"></i><span>Testimonials</span>
-            </a>
-          </li>
-          <li>
-            <a href="../year/">
-              <i class="bi bi-circle"></i><span>Years</span>
-            </a>
-          </li>
         </ul>
-      </li><!-- End Tables Nav -->
+      </li>
     </ul>
-
-  </aside><!-- End Sidebar-->
+  </aside>
