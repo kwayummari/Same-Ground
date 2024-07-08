4a0fd53885998dffeffa48f4975f455272f3b6bb
Kwayu Mmari <mmaridenis@gmail.com>
2024-07-08 12:48:03 +0300

    fixed menu

--- blog.php
+++ blog.php
@@ -5,74 +5,45 @@ $user_id = $_SESSION['id'];
 $status = $_SESSION['role'];
 $username = $_SESSION['name'];
 
-  $query="select * from blog";
-  $result4=mysqli_query($connect,$query);
-  $total_users = mysqli_num_rows($result4);
+$query = "select * from blog";
+$result4 = mysqli_query($connect, $query);
+$total_users = mysqli_num_rows($result4);
 echo $result4;
 
- ?>
+?>
 <!DOCTYPE html>
 <html>
 <?php include 'utils/head/index.php' ?>
+
 <body class="hidden-bar-wrapper">
-<div class="page-wrapper">
-	<?php include 'utils/header/header-others.php' ?>
-    <section class="page-title" style="background-image:url(images/background/12.jpg)">
-    	<div class="auto-container">
-			<div class="clearfix">
-				<div class="pull-left">
-					<div class="title">Welcome to our blog</div>
-					<h2>Latest News</h2>
-				</div>
-				<div class="pull-right">
-					<ul class="page-breadcrumb">
-						<li><a href="index-2.html">home</a></li>
-						<li>Latest News</li>
-					</ul>
-				</div>
-			</div>
-        </div>
-    </section>
-    <!--End Page Title-->
-	
-	<!-- Blog Page Section -->
-	<section class="blog-page-section">
-		<div class="auto-container">
-			<div class="row clearfix">
-			<?php   // LOOP TILL END OF DATA
-                                                    while($rows4=$result4->fetch_assoc())
-                                                         {
-                                                       ?>
-				<!-- News Block -->
-				<div class="news-block col-lg-6 col-md-12 col-sm-12">
-					<div class="inner-box">
-						<div class="image">
-							<a href="blog-single.php?id=<?php echo base64_encode($rows4['id']) ?>"><img src="cms/cv/<?php echo $rows4['image'];?>" alt="" /></a>
-						</div>
-						<div class="lower-content">
-							<ul class="post-meta">
-								<li>TWA</li>
-								<li><?php echo $rows4['date'];?></li>
-							</ul>
-							<h3><a href="blog-single.php?id=<?php echo base64_encode($rows4['id']) ?>"><?php echo $rows4['title'];?></a></h3>
-							<a href="blog-single.php?id=<?php echo base64_encode($rows4['id']) ?>" class="read-more">Read More</a>
-						</div>
+	<div class="page-wrapper">
+		<?php include 'utils/header/header-others.php' ?>
+		<section class="page-title" style="background-image:url(images/background/12.jpg)">
+			<div class="auto-container">
+				<div class="clearfix">
+					<div class="pull-left">
+						<div class="title">Welcome to our blog</div>
+						<h2>Latest News</h2>
+					</div>
+					<div class="pull-right">
+						<ul class="page-breadcrumb">
+							<li><a href="index-2.html">home</a></li>
+							<li>Latest News</li>
+						</ul>
 					</div>
 				</div>
-				<?php
-                                                     }
-                                                     ?>
 			</div>
+		</section>
+		<!--End Page Title-->
+
 		
-			
-		</div>
-	</section>
-	<!-- End Blog Page Section -->
-	<?php include 'utils/footer/index.php' ?>
-</div>
-<?php include 'utils/js/index.php' ?>
+		<!-- End Blog Page Section -->
+		<?php include 'utils/footer/index.php' ?>
+	</div>
+	<?php include 'utils/js/index.php' ?>
 
 </body>
 
 <!-- sawmall/about.html  26 Nov 2019 04:20:30 GMT -->
+
 </html>
