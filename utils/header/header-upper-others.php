c177b4440a840783a3eeda48564e024edc5c20ad
Kwayu Mmari <mmaridenis@gmail.com>
2024-07-08 13:15:38 +0300

    fixed menu

--- utils/header/header-upper-others.php
+++ utils/header/header-upper-others.php
@@ -21,17 +21,16 @@
 							<div class="upper-column info-box">
 								<div class="icon-box"><span class="flaticon-call"></span></div>
 								<ul>
-                                <li><span>+ 255 762 996 305 </span> <br> info@muslimwomen.or.tz</li>
-								</ul>
+                                <li><span>+ 255 762 996 305 </span> <br> samegroundtanzania@gmail.com</li></ul>
 							</div>
 							
 							<!--Info Box-->
 							<div class="upper-column info-box">
 								<ul class="social-icon-two">
-									<li><a href="https://www.facebook.com/muslimwomenawardstz/" style="background-color : #037c39; color: #ffffff"><span class="fa fa-facebook-f"></span></a></li>
-									<li><a href="https://www.instagram.com/muslimwomenawardstz/" style="background-color : #037c39; color: #ffffff"><span class="fa fa-instagram"></span></a></li>
-									<li><a href="https://www.twitter.com/muslimwomenawardstz/" style="background-color : #037c39; color: #ffffff"><span class="fa fa-twitter"></span></a></li>
-									<li><a href="https://www.youtube.com/muslimwomenawardstz/" style="background-color : #037c39; color: #ffffff"><span class="fa fa-youtube"></span></a></li>
+									<li><a href="https://www.facebook.com/samegroundtanzania/" style="background-color : #037c39; color: #ffffff"><span class="fa fa-facebook-f"></span></a></li>
+									<li><a href="https://www.instagram.com/samegroundtanzania/" style="background-color : #037c39; color: #ffffff"><span class="fa fa-instagram"></span></a></li>
+									<li><a href="https://www.twitter.com/samegroundtanzania/" style="background-color : #037c39; color: #ffffff"><span class="fa fa-twitter"></span></a></li>
+									<li><a href="https://www.youtube.com/samegroundtanzania/" style="background-color : #037c39; color: #ffffff"><span class="fa fa-youtube"></span></a></li>
 								</ul>
 							</div>
                         </div>
