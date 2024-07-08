025c6def8c2341ce7ce2645e00c80b291bba0d26
Kwayu Mmari <mmaridenis@gmail.com>
2024-07-08 11:54:13 +0300

    fixed menu

--- cms/connection/index.php
+++ cms/connection/index.php
@@ -1,4 +1,4 @@
 <?php 
-$connect=$db =mysqli_connect('localhost','u750269652_women','Women@2023','u750269652_women')
+$connect=$db =mysqli_connect('localhost','u750269652_sameground','SameGround@2024','u750269652_sameground')
 or die("connection Failed");
 ?>
